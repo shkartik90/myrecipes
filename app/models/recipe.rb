63633030f7ac619d95b69e1b6eab6dabccf9d378
shkartik90 <music22kartik@gmail.com>
2015-11-20 18:20:30 +0000

    add pagination

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe <ActiveRecord::Base
     
     belongs_to :chef
+    has_many :likes
+    
     validates :chef_id, presence: true
     
     validates :name, presence: true, length: {minimum:5, maximum: 100}
@@ -12,6 +14,17 @@ class Recipe <ActiveRecord::Base
     mount_uploader :picture, PictureUploader
     
     validate :picture_size
+    
+    default_scope -> { order(updated_at: :desc) }
+    
+    def thumbs_up_total
+        self.likes.where(like: true).size
+    end
+    
+    def thumbs_down_total
+        self.likes.where(like: false).size
+    end
+    
     
     private
         def picture_size
